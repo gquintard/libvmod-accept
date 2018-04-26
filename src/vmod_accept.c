9add2293bcbeb22f34d229a76260efff74948d7f
Guillaume Quintard <guillaume@varnish-software.com>
2018-04-26 18:48:18 +0200

    Compile without warnings

--- src/vmod_accept.c
+++ src/vmod_accept.c
@@ -1,11 +1,13 @@
 #include "config.h"
 
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include <errno.h>
 
-#include "cache/cache.h"
-#include "vqueue.h"
+#include <cache/cache.h>
+#include <miniobj.h>
+#include <vqueue.h>
 
 #include "vcc_accept_if.h"
 
